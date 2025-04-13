6a35e8f8c2e5b39fff41365cffa468aa38dc0411
vdhilly <65048905+vdhilly@users.noreply.github.com>
2025-04-13 17:49:47 +0200

    Update index.jsx

--- src/components/Works/Work/index.jsx
+++ src/components/Works/Work/index.jsx
@@ -39,7 +39,7 @@ function Work({ work }) {
                                 <li key={index}>{skill}</li>
                             ))}
                         </ul>
-                        <img src={`./assets/works/${image}`} alt={name} />
+                        <img src={`https://vdhilly.github.io/portfolio/assets/works/${image}`} alt={name} />
                         {github ? (
                             <a className="modale-github-logo" href={github} target="_blank"><img src={gitHubLogo} alt="github"/></a>
                         ) : null}
